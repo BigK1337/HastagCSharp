fa0d035eb8f7d2113aa7953827c2bc586de95aab
BigK1337 <kyleahicks@yahoo.com>
2014-06-26 11:01:01 -0400

    Update
    That's what she said.

---
+++ HelloOverusedMeme/HelloOverusedMeme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOverusedMeme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string readThis;
+            Console.WriteLine("Hello Internet!");
+            Console.WriteLine("Ramble down some notes here:");
+            readThis = Console.ReadLine();
+            Console.WriteLine(readThis);
+            Console.ReadLine();
+        }
+    }
+}
